016e135963892aa969ce68e2cf0fd8e670d1a2ba
Mikal Morello <mikalmorello@gmail.com>
2018-11-27 22:34:09 -0500

    THEME - Added Wall-e chat box

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
+// VARIABLES
+
+// FUNCTIONS
+
+// Set Focus
+/*function setFocus() {
+  var input = document.getElementById('chatInput');
+  input.focus();
+  console.log('is this working too');
+};
+
+setFocus(); 
+
+*/
+
+
 // add an event listener to the form to submit Dave's message
 
 // create a function for HAL to respond to Dave's messages with variable logic based upon
@@ -6,3 +22,8 @@
 // create a function for HAL to open the chat with "Good morning, Dave"
 
 // invoke the opening message
+
+// Add 
+
+// Set Focus
+
