083ce8944fe70a86945d1ff4c201735330d3dcf2
Mikal Morello <mikalmorello@gmail.com>
2018-11-29 22:51:57 -0500

    JS - Added basic Wall-e responses, allow user to submit from input when hitting enter

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,19 @@ let keyPhrases = [
   'name',
 ];
 
+
+let randomResponse = [
+  'Well, good morning, everybody, and welcome to day 255,642 aboard the Axiom.',
+  'M-O?',
+  'Waaaaaaaall-E',
+  '[stares]',
+  'Directive?',
+  'I didnt know we had a pool!',
+  'Ohh...',
+  'Leave the flying to us.',
+  '[Shrinks back in surprise]'
+];
+
 // FUNCTIONS
 
 // Wall-e Initial Response
@@ -29,13 +42,46 @@ let keyPhrases = [
 
 // Wall-e Response
 function walleResponse (){
-  chatOutput.innerText= 'testing';
+  // Check keyphrases
+  if(keyPhrase){
+    switch(keyPhrase) {
+        case 'eva':
+            chatOutput.innerText= 'Ee-Vah?';
+            break;
+        case 'wall-e':
+            chatOutput.innerText= 'Dirrrrr-ect-tivvve?';
+            break;
+        case 'garbage':
+            chatOutput.innerText= 'Where?';
+            break;
+        case 'cockroach':
+            chatOutput.innerText= 'Yes, my best friend is a cockroach.';
+            break;
+        case 'plant':
+            chatOutput.innerText= 'Give me the plant!';
+            break;
+        case 'space':
+            chatOutput.innerText= 'There is lots of space in space';
+            break;
+        case 'alone':
+            chatOutput.innerText= 'Yes, I am alone, but at least no one is making more trash';
+            break;
+        case 'name':
+            chatOutput.innerText= '[looks at his torso, which has his name on it] W--Wall-E';
+            break;
+        default:
+            chatOutput.innerText= 'I do not compute';
+    }
+  } else {
+    let randomNumber = Math.ceil(Math.random() * randomResponse.length);
+    chatOutput.innerText= randomResponse[randomNumber];
+  }
 }
 
 // User Input
 function userInput (){
   chatInputMessage = chatInput.value;
-  console.log(chatInputMessage);
+  //console.log(chatInputMessage);
   chatResponse.innerText = chatInputMessage;
   checkForKeyPhrases(chatInputMessage);
 }
@@ -43,10 +89,10 @@ function userInput (){
 // Check user input for key terms
 function checkForKeyPhrases(message) {
   for (let i = 0; i < keyPhrases.length; i++) {
-    console.log(message);
-    console.log([i]);
+    //console.log(message);
+    //console.log([i]);
     if(message.includes(keyPhrases[i])){
-      console.log(keyPhrases[i]);
+      //console.log(keyPhrases[i]);
       return keyPhrase = keyPhrases[i];
     }
   }
@@ -66,26 +112,26 @@ function checkUserChat (){
   }
 }
 
-/*// Submit message if user hits enter on input
-chatInput.addEventListener('keyup', function(event) {
-  console.log(event.keyCode);
-  event.preventDefault();
-  if (event.keyCode === 13) {
-    chatButton.click();
-  }
-});*/
-
 
 // Submit message if user clicks submit button
 chatButton.addEventListener('click', function() {
   console.log('button clicked');
   userInput();
   clearUserInput();
-  walleResponse();
   checkUserChat();
-  console.log('key phrase is: ' + keyPhrase); 
+  walleResponse(keyPhrase);
 });
 
+
+// Submit message if user hits enter on input
+chatInput.addEventListener('keyup', function(event) {
+  console.log(event.keyCode);
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    chatButton.click();
+    console.log('enter hit');
+  }
+});
 
 // create a function for HAL to respond to Dave's messages with variable logic based upon
 
