a7733315b650fc087ad00fdf326fb8bb09c3b406
Mikal Morello <mikalmorello@gmail.com>
2018-12-01 22:03:00 -0500

    JS - Updated logic, added question count based responses

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,6 @@ const chatForm = document.getElementById('chatForm');
 
 let chatInputMessage = '';
 let keyPhrase = '';
-
 let keyPhrases = [
   'eva',
   'wall-e',
@@ -18,16 +17,19 @@ let keyPhrases = [
   'space',
   'alone',
   'name',
+  'hi'
 ];
+
+var responseCount = 0;
 
 
 let randomResponse = [
-  'Well, good morning, everybody, and welcome to day 255,642 aboard the Axiom.',
+  'Well, good morning, and welcome to day 255,642 aboard the Axiom.',
   'M-O?',
   'Waaaaaaaall-E',
   '[stares]',
   'Directive?',
-  'I didnt know we had a pool!',
+  'umm ok. hey look! I didnt know we had a pool!',
   'Ohh...',
   'Leave the flying to us.',
   '[Shrinks back in surprise]'
@@ -42,8 +44,18 @@ let randomResponse = [
 
 // Wall-e Response
 function walleResponse (){
-  // Check keyphrases
-  if(keyPhrase){
+  
+  responseCount ++ 
+  console.log(responseCount);
+  
+  // Check chat count, keyphrases, 
+  if(responseCount === 5){
+    chatOutput.innerText= 'I am just a trash collecting robot, leave me alone!';
+  } else if(responseCount === 10){
+    chatOutput.innerText= 'My processer was not built for this type of stress testing...'; 
+  } else if(responseCount > 20){
+    chatOutput.innerText= 'Sorry you have exceeded your input limit. I need to recharge my solar battery, try again tomorrow';  
+  } else if(keyPhrase) {
     switch(keyPhrase) {
         case 'eva':
             chatOutput.innerText= 'Ee-Vah?';
@@ -52,7 +64,7 @@ function walleResponse (){
             chatOutput.innerText= 'Dirrrrr-ect-tivvve?';
             break;
         case 'garbage':
-            chatOutput.innerText= 'Where?';
+            chatOutput.innerText= 'Where? I love garbage.';
             break;
         case 'cockroach':
             chatOutput.innerText= 'Yes, my best friend is a cockroach.';
@@ -69,31 +81,34 @@ function walleResponse (){
         case 'name':
             chatOutput.innerText= '[looks at his torso, which has his name on it] W--Wall-E';
             break;
+        case 'hi':
+            chatOutput.innerText= 'Hi, how are you?';
+            break;
         default:
             chatOutput.innerText= 'I do not compute';
     }
   } else {
     let randomNumber = Math.ceil(Math.random() * randomResponse.length);
     chatOutput.innerText= randomResponse[randomNumber];
+    console.log('rando is ' + randomNumber);
   }
 }
 
 // User Input
 function userInput (){
   chatInputMessage = chatInput.value;
-  //console.log(chatInputMessage);
   chatResponse.innerText = chatInputMessage;
+  checkForQuestionMark(chatInputMessage);
   checkForKeyPhrases(chatInputMessage);
 }
 
 // Check user input for key terms
 function checkForKeyPhrases(message) {
   for (let i = 0; i < keyPhrases.length; i++) {
-    //console.log(message);
-    //console.log([i]);
     if(message.includes(keyPhrases[i])){
-      //console.log(keyPhrases[i]);
       return keyPhrase = keyPhrases[i];
+    } else {
+      keyPhrase = '';
     }
   }
 }
@@ -112,6 +127,16 @@ function checkUserChat (){
   }
 }
 
+// Check if user message has a question mark
+
+function checkForQuestionMark(message) {
+  if(message.includes('?')){
+      console.log('has question');
+  } 
+}
+
+
+// EVENTS
 
 // Submit message if user clicks submit button
 chatButton.addEventListener('click', function() {
@@ -133,7 +158,6 @@ chatInput.addEventListener('keyup', function(event) {
   }
 });
 
-// create a function for HAL to respond to Dave's messages with variable logic based upon
 
 
 
